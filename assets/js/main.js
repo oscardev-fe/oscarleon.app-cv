0c4eb395620cf0c6a7473287b2fdda95965c87d9
oscarl3on <oscarleon.alcatel@gmail.com>
2023-03-01 12:38:54 -0600

    orden de los elementos en el menu navegacion mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,17 @@
 /*==================== SHOW MENU ====================*/
-
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+    
+    // Validate that variables exist
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            // We add the show-menu class to the div tag with the nav__menu class
+            nav.classList.toggle('show-menu')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
 
 /*==================== REMOVE MENU MOBILE ====================*/
 
